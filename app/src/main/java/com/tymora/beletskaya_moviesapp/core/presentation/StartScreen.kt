15d235004f724dce2f1fa63d58746b6281fbc2d4
Tymora <dachabel@mail.ru>
2024-02-11 22:46:30 +0300

    Шрифты заголовка и кнопок

--- app/src/main/java/com/tymora/beletskaya_moviesapp/core/presentation/StartScreen.kt
+++ app/src/main/java/com/tymora/beletskaya_moviesapp/core/presentation/StartScreen.kt
@@ -5,11 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Movie
-import androidx.compose.material.icons.rounded.Upcoming
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -23,7 +19,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -66,7 +61,7 @@ fun StartScreen(navController: NavHostController) {
                 )
             },
             modifier = Modifier.shadow(2.dp),
-            colors = TopAppBarDefaults.smallTopAppBarColors(
+            colors = TopAppBarDefaults.topAppBarColors(
                 MaterialTheme.colorScheme.inverseOnSurface
             )
         )
